43baa0a8b16d0b3b285d23f02a8a43a64c61eb5d
erlend <wildifyme@gmail.com>
2024-08-11 11:21:52 +0200

    tailwind css now applying styles properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+/* 
+Erlend: Well, I think tailwind.config.js took precedence over the TypeScript config file,
+since Tailwind CSS got properly applied after adding to content here.
+*/
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+ 
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
