065425728e1e796f7bcb90153e9cf02abad69205
erlend <wildifyme@gmail.com>
2024-08-11 18:48:02 +0200

    it's done

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
   const [createTrainClicked, setCreateTrainClicked] = useState(false);
   const [trainIsLoading, setTrainIsLoading] = useState(false);
   const [toastMessage, setToastMessage] = useState("On and on and on ...");
+  const [trainCounter, setTrainCounter] = useState(0);
 
   function initialTrainButton(text:string, isClicked:boolean, loading:boolean){
     return isClicked ? (
@@ -46,7 +47,7 @@ export default function Home() {
   }
 
   function showTrain(trainCreated:boolean){
-    return trainCreated? (
+    return trainCreated ? (
       <ButtonGroup className="">
         <Button isIconOnly radius="full" className="w-96 h-96 text-emerald-300" onClick={(e) => {trainHandler(e)}}>
           <MaterialSymbolsTrain className="size-32" />
@@ -55,10 +56,18 @@ export default function Home() {
     ) : null
   }
 
+  function showTrainCounter(trainCreated:boolean){
+    return trainCreated ? (
+      <div className="text-xl">Path towards infinity ... <span className="text-amber-700 dark:text-amber-300">{ trainCounter }</span></div>
+    ) : null
+  }
+
   // Just a different way of defining a function
   const trainHandler = (e:any) => {
+    setTrainCounter(trainCounter + 1)
     const randomNumber = Math.round(Math.random() * 100)
-    console.log(`randomNumber: ${randomNumber}`)
+    // console.log(`randomNumber: ${randomNumber}`)
+    console.log(trainCounter)
     if(randomNumber > 90){
       toast.success('Oh! You found a golden nugget.', {
         style: {
@@ -92,7 +101,10 @@ export default function Home() {
 
         {initialTrainButton(initialTrainText, createTrainClicked, trainIsLoading)}
 
-        {showTrain(createTrainClicked)}
+        <div className="flex flex-col gap-4 items-center">
+          {showTrain(createTrainClicked)}
+          {showTrainCounter(createTrainClicked)}
+        </div>
 
       </div>
 
